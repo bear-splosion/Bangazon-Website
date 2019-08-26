6eaf33a195f06a3fedd94192b095e09c6576d176
Heather Cleland <heathercleland@gmail.com>
2019-08-26 13:36:58 -0500

    fix typo on ApplicationDbContext

--- Bangazon/Data/ApplicationDbContext.cs
+++ Bangazon/Data/ApplicationDbContext.cs
@@ -191,6 +191,7 @@ namespace Bangazon.Data {
                 {
                     OrderId = 1,
                     UserId = user.Id,
+                    PaymentTypeId = null
                 }
             );
 
