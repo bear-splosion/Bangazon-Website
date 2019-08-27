4691fe6500ec7cc39dc0e32e42b68e079f8fcc72
dek-haji <deeeqhaji@gmail.com>
2019-08-27 15:53:15 -0500

    accepted heather merge

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -14,22 +14,22 @@ namespace Bangazon.Models
         [Required]
         [DataType(DataType.Date)]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        [Display(Name = "Date Created")]
         public DateTime DateCreated {get;set;}
 
         [Required]
         [StringLength(255)]
-        [RegularExpression("^[a-zA-Z0-9 ]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+       
+
         public string Description { get; set; }
 
         [Required]
         [StringLength(55, ErrorMessage="Please shorten the product title to 55 characters")]
-        [RegularExpression("^[a-zA-Z0-9 ]*$")]
         public string Title { get; set; }
 
         [Required]
         [DisplayFormat(DataFormatString = "{0:C}")]
-        [Range(1, 10000)]
+        [Range(1, 100000)]
         public double Price { get; set; }
 
         [Required]
@@ -40,7 +40,6 @@ namespace Bangazon.Models
 
         public string City {get; set;}
 
-        [Display(Name = "Image Path")]
         public string ImagePath {get; set;}
 
         public bool Active { get; set; }
@@ -52,7 +51,6 @@ namespace Bangazon.Models
         [Display(Name="Product Category")]
         public int ProductTypeId { get; set; }
 
-        [Display(Name = "Product Category")]
         public ProductType ProductType { get; set; }
 
         public virtual ICollection<OrderProduct> OrderProducts { get; set; }
