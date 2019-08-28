4fb23ce3a58a85beb6ae43bcc6d873fb9506dade
Heather Cleland <heathercleland@gmail.com>
2019-08-28 14:40:31 -0500

    save changes before merging Logan's branch

--- Bangazon/Controllers/ProductsController.cs
+++ Bangazon/Controllers/ProductsController.cs
@@ -25,9 +25,10 @@ namespace Bangazon.Controllers
         }
         // End
 
-        // GET: Products
+        // GET: All Products that User is selling 
         public async Task<IActionResult> Index()
         {
+            
             // Heather added GetUserAsync method for getting current user
             var user = await GetUserAsync();
             // Heather updated this section below for getting current user
@@ -35,8 +36,17 @@ namespace Bangazon.Controllers
                 .Where(p => p.UserId == user.Id)
                 .Include(p => p.User)
                 .Include(p => p.ProductType);
-            return View(await applicationDbContext.ToListAsync());
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(await applicationDbContext.ToListAsync());
+            }
+
         }
+
 
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
