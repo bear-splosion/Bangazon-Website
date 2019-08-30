0c2666546dee0b6a04452bcd632dd7b481fdebea
Heather Cleland <heathercleland@gmail.com>
2019-08-30 12:19:51 -0500

    add back search functionality

--- Bangazon/Controllers/ProductsController.cs
+++ Bangazon/Controllers/ProductsController.cs
@@ -307,5 +307,20 @@ if (id != product.ProductId)
             return _userManager.GetUserAsync(HttpContext.User);
         }
         // End
+
+        //search bar method for finding products
+        public async Task<IActionResult> SearchProducts(string searchString)
+        {
+
+            var products = from p in _context.Product
+                           select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Title.Contains(searchString) || p.City.Contains(searchString));
+            }
+
+            return View(await products.ToListAsync());
+        }
     }
 }
