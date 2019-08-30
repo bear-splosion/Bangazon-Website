4ff309ad70be7c9fa63e4c75fc6562f92563ab30
Heather Cleland <heathercleland@gmail.com>
2019-08-30 10:24:14 -0500

    order history details showing

--- Bangazon/Models/OrderViewModels/OrderLineItem.cs
+++ Bangazon/Models/OrderViewModels/OrderLineItem.cs
@@ -1,5 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bangazon.Models.OrderViewModels {
     public class OrderLineItem {
+
+        [Key]
+        public int OrderId { get; set; }
+
         public Product Product { get; set; }
         public int Units { get; set; }
         public double Cost { get; set; }
